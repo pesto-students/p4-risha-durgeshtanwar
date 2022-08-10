35e1dd8322d0f47f7e9cad392526dbd23ed72a0a
Durgesh Tanwar <durgeshtanwar@Durgeshs-MacBook-Pro.local>
2022-08-10 15:41:05 +0530

    array of memoized function is now working

--- Week-3/script.js
+++ Week-3/script.js
@@ -1,12 +1,21 @@
 // assignment 1 memoize function.
 const memoize = () => {
-  let cache = {};
+  //let cache = {};
+  let array = [];
   return (...inputs) => {
-    if (JSON.stringify(cache.inputs) === JSON.stringify(inputs)) {
-      console.log("Fetching from cache");
-      console.log(cache.inputs);
-      return cache.results;
+    let res = array.find((input, i) => {
+      let i1 = input.inputs;
+      let i2 = inputs;
+      if (JSON.stringify(i1) === JSON.stringify(i2)) {
+        return input.results;
+      }
+    });
+    if (res) {
+      console.log("result fetched from cache");
+      console.log(res.results);
+      return res.results;
     } else {
+      let cache = {};
       console.log("calculating result");
       let result = inputs.reduce((prev, next) => {
         return prev + next;
@@ -14,15 +23,19 @@ const memoize = () => {
 
       cache.results = result;
       cache.inputs = inputs;
-      console.log(cache);
+      //  console.log(cache);
+      array.push(cache);
+      // console.log(array);
       return result;
     }
   };
 };
+
 const newAdd = memoize();
 // a simple memoized function to add something
-console.log(newAdd(14, 15, 23, 44));
-//console.log(newAdd(14,15));
-console.log(newAdd(14, 15, 23, 44));
-
-// returned function from memoizedAdd
+console.log(newAdd(12, 12, 33, 45, 23, 55, 23, 55));
+console.log(newAdd(10, 12));
+console.log(newAdd(12, 12, 33, 45, 23, 55));
+console.log(newAdd(12, 12, 33, 45, 23, 55, 23, 55));
+console.log(newAdd(100));
+console.log(newAdd(10, 12));
