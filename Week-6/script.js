54fddce1923a3fd8af9662b3bcdd291fabe19a44
Durgesh Tanwar <durgeshtanwar@Durgeshs-MacBook-Pro.local>
2022-09-18 23:27:39 +0530

    4 assignments done

--- Week-6/script.js
+++ Week-6/script.js
@@ -14,10 +14,10 @@ function sumArray(nums) {
   return globalMaxSum;
 }
 let a = [1, 2, 3, 4, -10];
-console.log(sumArray(a));
+//console.log(sumArray(a));
 
 let b = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-console.log(sumArray(b));
+//console.log(sumArray(b));
 
 // assignment 6.2
 
@@ -75,6 +75,53 @@ const spiralMatrix = (matrix) => {
   }
   return result;
 };
-console.log(...spiralMatrix(arr));
+//console.log(...spiralMatrix(arr));
 
 // assignment 6.3
+
+// Problem 6.3 Sort array of 0's,1's and 2's
+
+let arrayOfBinary = [0, 2, 1, 2, 0]; // [0,0,0,1,2,2]
+function sorting(problem) {
+  //console.log(problem[0]);
+
+  let arr0 = [];
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i < problem.length; i++) {
+    if (problem[i] === 0) {
+      console.log(problem[i]);
+      arr0.push(problem[i]);
+    } else if (problem[i] === 1) {
+      arr1.push(problem[i]);
+    } else if (problem[i] === 2) {
+      arr2.push(problem[i]);
+    }
+  }
+  //console.log(arr0);
+  return [...arr0, ...arr1, ...arr2];
+}
+// console.log(sorting(arrayOfBinary));
+
+// Assignment 6.4
+//Problem 6.4 : Best time to buy and sell stock
+
+let inputs = [7, 1, 5, 3, 6, 4];
+function bestTimeToBuyAndSell(inputs) {
+  let resultArray = [];
+  let buyingDay = inputs.indexOf(Math.min(...inputs)) + 1;
+  let buyingDayIndex = buyingDay - 1;
+  let buyingDayValue = inputs[buyingDayIndex];
+  let highestValue = Math.max(...inputs);
+  let indexOfHighestValue = inputs.indexOf(highestValue);
+  if (buyingDayIndex > indexOfHighestValue) {
+    const arr = inputs.slice(buyingDayIndex, inputs.length);
+    let highestValueInSlicedArray = Math.max(...arr);
+    // console.log(highestValueInSlicedArray);
+    return { profit: highestValueInSlicedArray - buyingDayValue };
+  } else {
+    return "0 profit";
+  }
+}
+console.log(bestTimeToBuyAndSell(inputs));
+console.log(bestTimeToBuyAndSell([7, 6, 4, 3, 1]));
